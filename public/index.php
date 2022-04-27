545d6731fd7ef3204086d3be8b96c1fe4fc44888
John Waithira <waithirajon@gmail.com>
2022-04-27 13:05:12 +0300

    pudate

--- public/index.php
+++ public/index.php
@@ -3,15 +3,41 @@
 use Dotenv\Dotenv;
 use Waithirajon\ImageUploadSiteMvc\app\core\Application;
 
+
+/*
+ * ------------------------------------------------------
+ * Registering Auto Loader
+ * ------------------------------------------------------*
+ *
+ **/
+
 require_once __DIR__."/../vendor/autoload.php";
+
+/*
+ * ------------------------------------------------------
+ * Get .env file to setup $_ENV (Environment)
+ * ------------------------------------------------------*
+ *
+ **/
 
 $dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
+/*
+*---------------------------------------------------------
+* Run The Application
+*---------------------------------------------------------*
+*/
 
 $app = new Application(dirname(__DIR__));
 
+/*
+ * ---------------------------------------------------------
+ * Include the Routing File
+ * ---------------------------------------------------------
+ * */
 require_once __DIR__."/../route/web.php";
 
+
 $app->run();
-?>
+
