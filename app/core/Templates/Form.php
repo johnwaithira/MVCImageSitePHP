8d56105f0c05907ea4f1c5a348d80871d1f2bfd8
John Waithira <waithirajon@gmail.com>
2022-04-27 13:12:11 +0300

    pudate

--- app/core/Templates/Form.php
+++ app/core/Templates/Form.php
@@ -4,5 +4,27 @@ namespace Waithirajon\ImageUploadSiteMvc\app\core\Templates;
 
 class Form
 {
+    public static function begin($action, $method)
+    {
+        echo sprintf('<form  action="%s" method = "%s" class=" p-10 m-t-20" enctype="multipart/form-data">', $action, $method);
+        return new Form();
+    }
 
+    public static function end()
+    {
+        echo "
+        <script>
+            if(window.history.replaceState)
+            {
+                window.history.replaceState(null, null, window.location.href)
+            }
+        
+        </script>
+        </form>";
+    }
+
+    public static function field($attribute)
+    {
+        return new Field($attribute);
+    }
 }
