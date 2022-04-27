88988aea19bb7de9f55d001367ace28d4622c020
John Waithira <waithirajon@gmail.com>
2022-04-27 12:23:37 +0300

    Edit

--- app/core/Application.php
+++ app/core/Application.php
@@ -1,8 +1,24 @@
 <?php
 
 namespace Waithirajon\ImageUploadSiteMvc\app\core;
-
+use Waithirajon\ImageUploadSiteMvc\app\core\Request;
+use Waithirajon\ImageUploadSiteMvc\app\core\Response;
+use Waithirajon\ImageUploadSiteMvc\app\core\Request;
 class Application
 {
+    public Response $response;
+    public Request $request;
+    public Router $router;
 
+    public function __construct()
+    {
+        $this->request = new Request();
+        $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
+    }
+
+    public function run()
+    {
+        echo $this->router->resolve();
+    }
 }
