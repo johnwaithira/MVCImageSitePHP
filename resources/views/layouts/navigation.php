9179a08b59e130622b7fd99bf0c46de630d3ca60
John Waithira <waithirajon@gmail.com>
2022-04-28 14:02:53 +0300

    update

--- resources/views/layouts/navigation.php
+++ resources/views/layouts/navigation.php
@@ -1,1 +1,25 @@
-<h1>Navigation Bar</h1>
+<header>
+    <nav>
+        <div class="wrapper">
+            <input  type="checkbox" name="checkNavBar"  id="checkNavBar">
+            <h1 class="logo"><a href="">instaGallery</a></h1>      
+            <div class="nav-items">
+               <div class="links">
+                    <li class="nav-links"><a id="a" href="#">Home</a></li>
+                    <li class="nav-links"><a id="a" href="#">About</a></li>
+                    <li class="nav-links"><a id="a" href="#">Projects</a></li>
+               </div>
+               <div class="link-btns">
+                    <a href="login" ><button class="bg-inherit">Login</button></a>
+                    <a href="create"><button class="bg-inherit">Create Acc</button></a>     
+               </div>
+            </div>
+            <label id="humberger" for="checkNavBar">
+                <div class="menu"></div>
+                <div class="menu mid"></div>
+                <div class="menu"></div>
+            </label>
+        </div>
+    </nav>
+    <hr style="border: 1px solid #f8f8f8">
+</header>
